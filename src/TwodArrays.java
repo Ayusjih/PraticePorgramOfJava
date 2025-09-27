79f94530e0449cf05f7b58b8fc2d2fbeafd06e0d
Ayush Ojha <ayushojha992005@gmail.com>
2025-09-27 19:38:51 +0530

    how to define

--- src/TwodArrays.java
+++ src/TwodArrays.java
@@ -1,8 +1,14 @@
+import java.util.Arrays;
+
 public class TwodArrays {
 
     public static void main(String[] args) {
 
         int[][] arr = new int[40][50];
+        int[][] arr1 = {{4,6,7,4},{8,7,1,5},
+        {8,9,5,3}};
+
+        System.out.println(Arrays.toString(arr1));
 
     }
 
