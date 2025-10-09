c99df6a4cbc31d4e1d823f774831b605e69fc82f
Ayush Ojha <ayushojha992005@gmail.com>
2025-10-09 19:18:34 +0530

    DemoMethods

--- src/Methods_in_Java/MethodDemo3.java
+++ src/Methods_in_Java/MethodDemo3.java
@@ -4,9 +4,10 @@ import javax.swing.plaf.synth.SynthOptionPaneUI;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
 
 public class MethodDemo3 {
-
+    static Scanner sc = new Scanner(System.in);
     public static void Sumbmission(int a, int b)
     {
         System.out.println(a+b);
@@ -18,19 +19,35 @@ public class MethodDemo3 {
         System.out.println(Arrays.toString(a));
         System.out.println(List);
     }
+    public  static  void   changeName(String name)
+    {
+        name = sc.nextLine();
+        System.out.println("In Method "+name);
+    }
 
     public static void main(String[] args) {
-        List<Integer>list1 = new ArrayList<>(5);
-        list1.add(2);
-        list1.add(2);
-        list1.add(3);
-        list1.add(4);
-        list1.add(5);
-        int[] a={2,5,6,7,8};
-        Sumbmission(12,14);
-        Array(list1,a);
+
+
+        String name = "Ayush";
+        System.out.println("Before Method "+name);
+        changeName(name);
+        System.out.println("After Method "+name);
+
+//        List<Integer>list1 = new ArrayList<>(5);
+//        list1.add(2);
+//        list1.add(2);
+//        list1.add(3);
+//        list1.add(4);
+//        list1.add(5);
+//        int[] a={2,5,6,7,8};
+//        Sumbmission(12,14);
+//        Array(list1,a);
 
     }
+
+
+
+
 
 
 
