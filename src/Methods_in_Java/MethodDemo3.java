cf9063db591b1c4e339338d9e0b9b5a5c4dcbb09
Ayush Ojha <ayushojha992005@gmail.com>
2025-10-09 18:50:24 +0530

    Demoof Methods

---
+++ src/Methods_in_Java/MethodDemo3.java
@@ -0,0 +1,19 @@
+package Methods_in_Java;
+
+public class MethodDemo3 {
+
+
+
+    public static void main(String[] args) {
+
+        Sumbmission(12,14);
+
+
+    }
+
+    public static void Sumbmission(int a, int b)
+    {
+        System.out.println(a+b);
+    }
+
+}
