442bac8527c1ac3a9128575257cef283e955afa8
Ayush Ojha <ayushojha992005@gmail.com>
2025-10-11 19:15:27 +0530

    method demo 1

---
+++ src/OOP/Demo1.java
@@ -0,0 +1,43 @@
+package OOP;
+
+
+class demo{
+    private String name;
+     private int rollNo;
+     private float marks;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    public void setRollNo(int rollNo) {
+        this.rollNo = rollNo;
+    }
+
+    public void setMarks(float marks) {
+        this.marks = marks;
+    }
+}
+
+
+public class Demo1 {
+    public static void main(String[] args) {
+
+        demo ayush= new demo();
+        //ayush.name="Ayush";
+        //ayush.marks=124;
+        //ayush.rollNo=124123;
+        //System.out.println(ayush.name+" "+  ayush.marks+" "+ayush.rollNo );
+        ayush.setName("OJHA");
+        System.out.println(ayush.getName());
+
+    }
+
+}
+
+
