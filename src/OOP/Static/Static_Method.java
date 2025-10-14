f25663e97d45907a422eaca675e1a191ae0549e4
Ayush Ojha <ayushojha992005@gmail.com>
2025-10-14 19:55:41 +0530

    Static Example

---
+++ src/OOP/Static/Static_Method.java
@@ -0,0 +1,37 @@
+package OOP.Static;
+
+
+
+
+public class Static_Method {
+
+    public static void main(String[] args) {
+
+        Static_Method.Greet();
+        Greet();dwdwdwrwfvfffevrbgrefcdcsssssssssssssscccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
+       Static_Method obj = new Static_Method();
+       obj.Greeting();
+
+    }
+
+
+    static void Greet(){
+        System.out.println("Im from Static method:");
+        Static_Method obj1 = new Static_Method();
+        obj1.Greeting();
+    }
+    void Greeting()
+    {
+        System.out.println("Im from non static method");
+
+    }
+
+    void fun(){
+        Greeting();
+    }
+    void  fun2(){
+        Greet();
+    }
+
+
+}
