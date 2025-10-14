0d7193a118284be426c73af865b8f4baf3daba87
Ayush Ojha <ayushojha992005@gmail.com>
2025-10-14 19:28:48 +0530

    demmo 4

--- src/OOP/Static/Main.java
+++ src/OOP/Static/Main.java
@@ -6,9 +6,10 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Human   H1=new Human(15,"Ayush","Male",true);
+        Human   H1=new Human(20,"Ayush","Male",true);
         Human   H2=new Human(16,"Ayushi","Female",true);
         Human   H3=new Human(17,"Ayu","Male",false);
+        Human   H4=new Human(19,"Ishu Sharma","Female",false);
         System.out.println(Human.population);
 
 
