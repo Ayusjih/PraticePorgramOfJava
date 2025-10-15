07b6c627e41f4cdfd3a8d4b8d065e748cb53400f
Ayush Ojha <ayushojha992005@gmail.com>
2025-10-15 23:52:00 +0530

    Methods

---
+++ src/Methods_in_Java/GeometryDemo.java
@@ -0,0 +1,24 @@
+package Methods_in_Java;
+
+import Methods_in_Java.Class;
+
+public class GeometryDemo {
+
+    public static void main(String[] args) {
+        // We can create objects of concrete classes.
+        Shape myRectangle = new Rectangle("Blue", 10.0, 5.0);
+        Shape myCircle = new Circle("Red", 7.0);
+        Shape anotherCircle = new Circle("Green", 10); // Uses the overloaded constructor
+
+        // Create an array of Shape to demonstrate polymorphism.
+        Shape[] shapes = {myRectangle, myCircle, anotherCircle};
+
+        System.out.println("--- Displaying Shape Information ---");
+
+        // Loop through the array and call the same method on different objects.
+        for (Shape shape : shapes) {
+            shape.displayInfo(); // Polymorphism in action!
+            System.out.println("--------------------");
+        }
+    }
+}
