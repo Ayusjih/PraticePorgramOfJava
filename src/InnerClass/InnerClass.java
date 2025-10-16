22f800c4810e08f6147d4425efc39ad1b5f2cba1
Ayush Ojha <ayushojha992005@gmail.com>
2025-10-16 19:13:10 +0530

    inner class demo

---
+++ src/InnerClass/InnerClass.java
@@ -0,0 +1,22 @@
+package InnerClass;
+
+public class InnerClass {
+    static class test{
+        String name;
+        public test(String name)
+        {
+            this.name = name;
+        }
+    }
+
+    public static void main(String[] args) {
+        test a = new test("Ayush");
+        test b = new test("Aman");
+        System.out.println(a.name);
+        System.out.println(b.name);
+    }
+
+
+}
+
+
