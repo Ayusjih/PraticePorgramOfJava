3c59e8b9ff3b48c175f3d5fff6fea382249d6b1e
Ayush Ojha <ayushojha992005@gmail.com>
2025-10-30 19:42:26 +0530

    OPPs practice

---
+++ src/Polymorphism/Shapes.java
@@ -0,0 +1,29 @@
+package Polymorphism;
+
+public class Shapes {
+    void area()
+    {
+        System.out.println("Area is :" );
+    }
+
+}
+
+class Cricle extends Shapes{
+    void area()
+    {
+        System.out.println("Area of circle: ");
+    }
+}
+class Tringle extends Shapes{
+    void area()
+    {
+        System.out.println("Area of Tringle: ");
+    }
+}
+
+class Square extends Shapes{
+    void area()
+    {
+        System.out.println("Area of circle: ");
+    }
+}
