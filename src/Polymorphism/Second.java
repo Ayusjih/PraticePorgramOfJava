1a1d4596fd174dd1fed49aefe334fceb1570c25f
Ayush Ojha <ayushojha992005@gmail.com>
2025-10-30 19:27:43 +0530

    OPPs practice

---
+++ src/Polymorphism/Second.java
@@ -0,0 +1,24 @@
+package Polymorphism;
+
+public class Second {
+
+    void sum(int a,int b)
+    {
+        System.out.println("A");
+    }
+    void sum(int a , int b, int c)
+    {
+        System.out.println("B");
+    }
+    void sum(double a, double b)
+    {
+        System.out.println("C");
+    }
+
+    public static void main(String[] args) {
+
+        Second S= new Second();
+
+        S.sum(2,2.5);
+    }
+}
