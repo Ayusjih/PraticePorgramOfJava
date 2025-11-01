9848fe958d0e58d1ba5fbc94490788384fa32d43
Ayush Ojha <ayushojha992005@gmail.com>
2025-11-01 19:29:22 +0530

    OPPs practice// runtime polymorhism

---
+++ src/Polymorphism/Overideing.java
@@ -0,0 +1,21 @@
+package Polymorphism;
+
+public class Overideing {
+    int num;
+    Overideing(int num){
+        this.num = num;
+
+    }
+    public String toString(){
+        return "num"+" = "+num;
+    }
+
+
+    public static void main(String[] args) {
+        Overideing O1 = new Overideing(23);
+        System.out.println(O1);
+    }
+
+}
+
+
