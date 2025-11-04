4ff41e6fffdd9b8cfbb2103ffb9664d3b3824f30
Ayush Ojha <ayushojha992005@gmail.com>
2025-11-04 19:11:16 +0530

    OPPs practice//

---
+++ src/Encapsulation/First.java
@@ -0,0 +1,30 @@
+package Encapsulation;
+
+public class First {
+
+    private int num;
+    private String Name;
+    public First(int num, String Name){
+        this.num = num;
+        this.Name = Name;
+    }
+
+    void display()
+    {
+        System.out.println(" "+num);
+    }
+
+
+    public int getNum() {
+        return this.num;
+    }
+
+    public void setNum(int num) {
+        this.num = num;
+    }
+
+
+    public void setName(String name) {
+        this.Name = name;
+    }
+}
