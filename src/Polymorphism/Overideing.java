d645e2d81b5bec63b2cb789d972c966d6beaabff
Ayush Ojha <ayushojha992005@gmail.com>
2025-11-05 21:45:45 +0530

    OPPs practice//

--- src/Polymorphism/Overideing.java
+++ src/Polymorphism/Overideing.java
@@ -4,8 +4,7 @@ public class Overideing {
     int num;
     Overideing(int num){
         this.num = num;
-    }
-    public String toString(){
+    }    public String toString(){
         return "num"+" = "+num;
     }
     public static void main(String[] args) {
