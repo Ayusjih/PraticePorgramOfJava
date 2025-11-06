d874617bc17c7f9f597d52a8f748930714f4b898
Ayush Ojha <ayushojha992005@gmail.com>
2025-11-06 19:32:24 +0530

    OPPs practice//

--- src/HackerRank/Kangaroo2.java
+++ src/HackerRank/Kangaroo2.java
@@ -5,27 +5,21 @@ public class Kangaroo2 {
 
     public static String kangaroo(int x1, int v1, int x2, int v2) {
 
-        int y1 = x1+v1;
-        int y2 = x2+v2;
-        if(x1<10000 && x2<10000){
-            if(y1!=y2)
-            {
-                x1= y1;
-                x2 = y2;
-                kangaroo(x1, v1, x2, v2);
-
+           int y1=x1;
+           int y2=x2;
+        for(int i =0;i<10000;i++) {
+            if (y1 == y2) {
+                System.out.println("yes");
             }
 
-            else{
-                System.out.println("YES");
+            y1 = y1 + v1;
+            y2 = y2 + v2;
+
+            if ((v1 > v2 && x1 > x2) || (v2 > v1 && x2 > x1)) {
+                break;
             }
-
         }
-        else{
-            System.out.println("NO");
-        }
-
-        return " ";
+        return "NO";
 
     }
 
