97590704d42d4dc98ae642577d929135db9e2bd9
Ayush Ojha <ayushojha992005@gmail.com>
2025-11-11 19:15:29 +0530

    JAVA LIST

---
+++ src/Interface/Dog.java
@@ -0,0 +1,12 @@
+package Interface;
+
+public class Dog implements InterfaceOne{
+
+    public void eat(){
+        System.out.println("Dog is eating");
+    }
+    public void sleep()
+    {
+        System.out.println("Dog is sleeping");
+    }
+}
