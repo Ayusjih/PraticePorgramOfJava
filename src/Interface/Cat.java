98e27e250269459bf57c7c7f2d9bd08553c536d5
Ayush Ojha <ayushojha992005@gmail.com>
2025-11-11 19:15:21 +0530

    JAVA LIST

---
+++ src/Interface/Cat.java
@@ -0,0 +1,12 @@
+package Interface;
+
+public class Cat implements InterfaceOne {
+
+    public void eat(){
+        System.out.println("Cat is eating");
+    }
+    public void sleep()
+    {
+        System.out.println("Cat is sleeping");
+    }
+}
