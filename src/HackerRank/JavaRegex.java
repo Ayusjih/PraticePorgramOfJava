1cf58bb8517ad3193f6b221609a36a545e9e95f3
Ayush Ojha <ayushojha992005@gmail.com>
2025-11-12 21:20:53 +0530

    JAVA pratice

---
+++ src/HackerRank/JavaRegex.java
@@ -0,0 +1,16 @@
+package HackerRank;
+
+public class JavaRegex {
+
+
+}
+
+class MyRegex {
+    // Regular expression pattern for a valid IP address
+    String pattern =
+            "^(\\d{1,2}|(0|1)\\d{2}|2[0-4]\\d|25[0-5])\\."
+                    + "(\\d{1,2}|(0|1)\\d{2}|2[0-4]\\d|25[0-5])\\."
+                    + "(\\d{1,2}|(0|1)\\d{2}|2[0-4]\\d|25[0-5])\\."
+                    + "(\\d{1,2}|(0|1)\\d{2}|2[0-4]\\d|25[0-5])$";
+}
+
