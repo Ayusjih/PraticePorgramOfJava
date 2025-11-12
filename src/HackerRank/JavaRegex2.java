e08afc645aa33dca63ac92e073a221bda7190213
Ayush Ojha <ayushojha992005@gmail.com>
2025-11-12 21:21:07 +0530

    JAVA pratice

---
+++ src/HackerRank/JavaRegex2.java
@@ -0,0 +1,32 @@
+package HackerRank;
+import java.math.BigDecimal;
+import java.util.*;
+class JavaRegex2{
+
+    public static void main(String []args){
+        //Input
+        Scanner sc= new Scanner(System.in);
+        int n=sc.nextInt();
+        String []s=new String[n+2];
+        for(int i=0;i<n;i++){
+            s[i]=sc.next();
+        }
+        sc.close();
+
+        Arrays.sort(s, 0, n, new Comparator<String>() {
+            public int compare(String a, String b) {
+                BigDecimal bd1 = new BigDecimal(a);
+                BigDecimal bd2 = new BigDecimal(b);
+                // Descending order
+                return bd2.compareTo(bd1);
+            }
+        });
+
+        //Output
+        for(int i=0;i<n;i++)
+        {
+            System.out.println(s[i]);
+        }
+    }
+
+}
