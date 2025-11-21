577085e469a2e4f18135024f32e2a87d07e4ffd3
Ayush Ojha <ayushojha992005@gmail.com>
2025-11-21 22:04:08 +0530

    DSA LEET-CODE 15

---
+++ src/DataStructureAlgorithm/LEETCODE/Sum3.java
@@ -0,0 +1,22 @@
+package DataStructureAlgorithm.LEETCODE;
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<Integer> list1 = new List<>;
+        for(int i=0;i<nums.length();i++)
+        {
+            for(int j=0;j<nums.length();j++)
+            {
+                for(int k=0;k<nums.length();k++)
+                {
+                    System.out.println(nums[i]+" "+nums[j]+" "+nums[k]);
+
+
+                }
+
+            }
+
+        }
+
+
+    }
+}
