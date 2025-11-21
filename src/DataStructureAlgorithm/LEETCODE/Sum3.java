ad463eb39ff8cd9aee1b3e082e05a2dd2e1d9086
Ayush Ojha <ayushojha992005@gmail.com>
2025-11-22 00:04:03 +0530

    DSA LEET-CODE 15

--- src/DataStructureAlgorithm/LEETCODE/Sum3.java
+++ src/DataStructureAlgorithm/LEETCODE/Sum3.java
@@ -1,22 +1,39 @@
 package DataStructureAlgorithm.LEETCODE;
-class Solution {
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+class Sum3 {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<Integer> list1 = new List<>;
-        for(int i=0;i<nums.length();i++)
-        {
-            for(int j=0;j<nums.length();j++)
-            {
-                for(int k=0;k<nums.length();k++)
-                {
-                    System.out.println(nums[i]+" "+nums[j]+" "+nums[k]);
-
-
+        List<List<Integer>> result = new ArrayList<>();
+        List<List<Integer>> result2 = new ArrayList<>();
+        for (int i = 0; i < nums.length - 2; i++) {
+            for (int j = i + 1; j < nums.length - 1; j++) {
+                for (int k = j + 1; k < nums.length; k++) {
+                    List<Integer> temp = Arrays.asList(nums[i], +nums[j], nums[k]);
+                    result.add(temp);
                 }
-
+            }
+        }
+        for (int i = 0; i < result.size(); i++) {
+            List<Integer> triplet = result.get(i);
+            int a = triplet.get(0);
+            int b = triplet.get(1);
+            int c = triplet.get(2);
+            if (a + b + c == 0) {
+                //System.out.println("Found zero-sum triplet: " + triplet);
+                result2.add(triplet);
             }
 
         }
+        System.out.println(result2);
+        return result2;
+    }
 
-
+    public static void main(String[] args) {
+        int[] a = {1, 0, 1, 2, -1, -4};
+        Sum3 s = new Sum3();
+        s.threeSum(a);
     }
 }
