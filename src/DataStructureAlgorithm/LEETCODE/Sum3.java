e361beaafb1a58a20bc4c3b531239c113f5e0c88
Ayush Ojha <ayushojha992005@gmail.com>
2025-11-22 14:50:07 +0530

    DSA LEET-CODE 15

--- src/DataStructureAlgorithm/LEETCODE/Sum3.java
+++ src/DataStructureAlgorithm/LEETCODE/Sum3.java
@@ -2,6 +2,7 @@ package DataStructureAlgorithm.LEETCODE;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 class Sum3 {
@@ -11,8 +12,11 @@ class Sum3 {
         for (int i = 0; i < nums.length - 2; i++) {
             for (int j = i + 1; j < nums.length - 1; j++) {
                 for (int k = j + 1; k < nums.length; k++) {
-                    List<Integer> temp = Arrays.asList(nums[i], +nums[j], nums[k]);
-                    result.add(temp);
+                    List<Integer> temp = Arrays.asList(nums[i], nums[j], nums[k]);
+                    Collections.sort(temp);
+                    if(temp.get(i)!=temp.get(i+1)){
+                    result.add(temp);}
+
                 }
             }
         }
@@ -22,7 +26,7 @@ class Sum3 {
             int b = triplet.get(1);
             int c = triplet.get(2);
             if (a + b + c == 0) {
-                //System.out.println("Found zero-sum triplet: " + triplet);
+                System.out.println("Found zero-sum triplet: " + triplet);
                 result2.add(triplet);
             }
 
@@ -32,7 +36,7 @@ class Sum3 {
     }
 
     public static void main(String[] args) {
-        int[] a = {1, 0, 1, 2, -1, -4};
+        int[] a = {-1, 0, 1, 2, -1, -4};
         Sum3 s = new Sum3();
         s.threeSum(a);
     }
