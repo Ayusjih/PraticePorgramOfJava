6ab8fdba542110cf9f01738617fa1e597f194b43
Ayush Ojha <ayushojha992005@gmail.com>
2025-11-22 14:49:46 +0530

    DSA LEET-CODE REMOVE DUPLICATE

---
+++ src/DataStructureAlgorithm/main.java
@@ -0,0 +1,37 @@
+package DataStructureAlgorithm;
+
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.List;
+
+class Solution {
+
+    public int removeDuplicates(int[] nums) {
+        int k=1;
+        Arrays.sort(nums);
+        for(int i = 1;i<nums.length;i++)
+        {
+            if(nums[i]!=nums[i-1])
+            {
+                nums[k]=nums[i];
+                k++;
+            }
+            else{
+                continue;
+            }
+        }
+        System.out.println(k);
+        return k;
+    }
+}
+
+public class main {
+
+    public static void main(String[] args) {
+        int[] arrays={1,2,3,4,7,77,1,3,4};
+        Solution s = new Solution();
+        s.removeDuplicates(arrays);
+
+    }
+}
