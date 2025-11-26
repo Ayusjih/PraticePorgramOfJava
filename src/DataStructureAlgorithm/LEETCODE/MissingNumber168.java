5cc77774d00b7e390ff7faea5dd704f21eab82d4
Ayush Ojha <ayushojha992005@gmail.com>
2025-11-26 19:20:09 +0530

    168 MissingNumber168

---
+++ src/DataStructureAlgorithm/LEETCODE/MissingNumber168.java
@@ -0,0 +1,30 @@
+package DataStructureAlgorithm.LEETCODE;
+
+public class MissingNumber168 {
+
+        public int missingNumber(int[] nums) {
+
+            int res = 0;
+            int j =nums.length;
+            int sum = (j*(j+1))/2;
+
+            for(int i =0;i<=nums.length-1;i++)
+            {
+                res= res +nums[i];
+            }
+
+            res = sum-res;
+            return res;
+
+        }
+
+
+    public static void main(String[] args) {
+
+            int[] are ={0,3,1};
+            MissingNumber168 m =new  MissingNumber168();
+        m.missingNumber(are);
+    }
+
+
+}
