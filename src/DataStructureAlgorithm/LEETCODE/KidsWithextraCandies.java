5f14fa3b9e6bed525c1616fac1ffe0a6c56467ff
Ayush Ojha <ayushojha992005@gmail.com>
2025-11-29 18:49:34 +0530

    KidsWithextraCandies

---
+++ src/DataStructureAlgorithm/LEETCODE/KidsWithextraCandies.java
@@ -0,0 +1,46 @@
+package DataStructureAlgorithm.LEETCODE;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class KidsWithextraCandies {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        List<Boolean> List1 = new ArrayList<>();
+        int n=candies.length,max=0;
+        int[] res=new int[n];
+
+        for(int i =0;i<candies.length;i++)
+        {
+            res[i]+=extraCandies;
+
+        }
+
+
+        for(int i=0;i<candies.length;i++)
+        {
+            if(max <candies[i])
+            {max=candies[i];}
+
+        }
+
+        for(int i=0;i<n;i++)
+        {
+            if(max<=res.get(i))
+            {
+                List1.add(true);
+            }
+            else
+            {
+                List1.add(false);
+            }
+        }
+        System.out.println(res);
+        return List1;
+
+    }
+
+    public static void main(String[] args) {
+        KidsWithextraCandies kd =new KidsWithextraCandies();
+        kd.kidsWithCandies();
+    }
+}
