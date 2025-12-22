51e99a5c3aa07e504ba9543e8ab335b35e89ce1a
Ayush Ojha <ayushojha992005@gmail.com>
2025-12-22 22:19:54 +0530

    ugly number

---
+++ src/DataStructureAlgorithm/LEETCODE/uglynumber.java
@@ -0,0 +1,18 @@
+package DataStructureAlgorithm.LEETCODE;
+
+public class uglynumber {
+
+    public boolean isUgly(int n) {
+        if (n <= 0) {
+            return false;
+        }
+
+        for (int factor : new int[]{2, 3, 5}) {
+            while (n % factor == 0) {
+                n /= factor;
+            }
+        }
+
+        return n == 1;
+    }
+}
