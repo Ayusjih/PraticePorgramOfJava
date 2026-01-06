480c47571b6e9332a9853b51a324e87204e8e643
Ayush Ojha <ayushojha992005@gmail.com>
2026-01-06 21:51:25 +0530

    1630

---
+++ src/DataStructureAlgorithm/LEETCODE/minSubArrayLen.java
@@ -0,0 +1,28 @@
+package DataStructureAlgorithm.LEETCODE;
+
+public class minSubArrayLen {
+    public int minSubArrayLen(int target, int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+
+        int minLength = Integer.MAX_VALUE;
+        int currentSum = 0;
+        int left = 0;
+
+
+        for (int right = 0; right < nums.length; right++) {
+            currentSum += nums[right];
+            while (currentSum >= target) {
+
+                int currentWindowSize = right - left + 1;
+                minLength = Math.min(minLength, currentWindowSize);
+
+                currentSum -= nums[left];
+                left++;
+            }
+        }
+        return (minLength == Integer.MAX_VALUE) ? 0 : minLength;
+    }
+
+}
