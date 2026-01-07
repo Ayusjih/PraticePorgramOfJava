cf473d7a7891db34043c719279b5316ecc61e7dc
Ayush Ojha <ayushojha992005@gmail.com>
2026-01-07 19:17:27 +0530

    01 two sum hash map

---
+++ src/DataStructureAlgorithm/LEETCODE/TwoSumWithHashMap.java
@@ -0,0 +1,34 @@
+package DataStructureAlgorithm.LEETCODE;
+
+import java.util.HashMap;
+
+public class TwoSumWithHashMap {
+
+        public int[] twoSum(int[] nums, int target) {
+
+            HashMap<Integer,Integer> mpp = new HashMap<>();
+            int [] ans = new int[2];
+            int n = nums.length;
+
+            ans[0]=-1;
+            ans[1]=-1;
+
+            for(int i =0;i<n;i++)
+            {
+                mpp.put(nums[i],i);
+            }
+
+            for(int i=0;i<n;i++)
+            {
+                int rem = target - nums[i];
+                if(mpp.containsKey(rem) && mpp.get(rem)!= i)
+                {
+                    ans[0]=i;
+                    ans[1]=mpp.get(rem);
+                    break;
+                }
+            }
+
+            return ans;
+        }
+    }
