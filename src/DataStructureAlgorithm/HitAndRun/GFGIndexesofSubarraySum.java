253e9d5b9247e43820ec35f317a753364fddf72e
Ayush Ojha <ayushojha992005@gmail.com>
2026-01-23 21:52:08 +0530

    gfg

---
+++ src/DataStructureAlgorithm/HitAndRun/GFGIndexesofSubarraySum.java
@@ -0,0 +1,38 @@
+package DataStructureAlgorithm.HitAndRun;
+
+class Solution {
+    static ArrayList<Integer> subarraySum(int[] arr, int target) {
+
+        int n = arr.length;
+
+
+        ArrayList<Integer> list= new ArrayList<>();
+        HashMap<Long, Integer> mpp = new HashMap<>();
+        long prefix =0;
+
+        for(int i = 0;i<n;i++)
+        {
+            prefix +=arr[i];
+            if(prefix == target)
+            {
+                list.add(1);
+                list.add(i+1);
+                return list;
+            }
+
+            long rem = prefix - target;
+            if(mpp.containsKey(rem))
+            {
+                list.add(mpp.get(rem)+2);
+                list.add(i+1);
+                return list;
+            }
+
+            mpp.put(prefix,i);
+
+        }
+        list.add(-1);
+        return list;
+
+    }
+}
