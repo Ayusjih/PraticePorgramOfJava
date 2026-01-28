0bd78e9e365490eb3ebbc7b3a13651599dab5ad3
Ayush Ojha <ayushojha992005@gmail.com>
2026-01-28 19:47:50 +0530

    389

--- src/DataStructureAlgorithm/LEETCODE/FindTheDifference.java
+++ src/DataStructureAlgorithm/LEETCODE/FindTheDifference.java
@@ -4,7 +4,6 @@ public class FindTheDifference {
 
         public char findTheDifference(String s, String t) {
             int xor = 0;
-
             for (int i = 0; i < s.length(); i++) {
                 xor ^= s.charAt(i);
             }
