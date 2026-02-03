bf4c58a204f223251be1d6be554d5983e641cc47
Ayush Ojha <ayushojha992005@gmail.com>
2026-02-03 20:57:32 +0530

    238

--- src/DataStructureAlgorithm/LEETCODE/ProductOfSubArray238.java
+++ src/DataStructureAlgorithm/LEETCODE/ProductOfSubArray238.java
@@ -5,8 +5,6 @@ public class ProductOfSubArray238 {
         public int[] productExceptSelf(int[] nums) {
             int n = nums.length;
             int[] answer = new int[n];
-
-
             answer[0] = 1;
             for (int i = 1; i < n; i++) {
                 answer[i] = answer[i - 1] * nums[i - 1];
