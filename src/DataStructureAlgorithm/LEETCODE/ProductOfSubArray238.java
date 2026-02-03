0be82a6b4303493a548b000f24c3e7a6a7a42fbc
Ayush Ojha <ayushojha992005@gmail.com>
2026-02-03 20:58:39 +0530

    238

--- src/DataStructureAlgorithm/LEETCODE/ProductOfSubArray238.java
+++ src/DataStructureAlgorithm/LEETCODE/ProductOfSubArray238.java
@@ -9,8 +9,6 @@ public class ProductOfSubArray238 {
             for (int i = 1; i < n; i++) {
                 answer[i] = answer[i - 1] * nums[i - 1];
             }
-
-
             int suffixProduct = 1;
             for (int i = n - 1; i >= 0; i--) {
                 answer[i] *= suffixProduct;
