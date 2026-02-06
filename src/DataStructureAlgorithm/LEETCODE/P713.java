1782c14b0e368072331e8f1b267ec77442125127
Ayush Ojha <ayushojha992005@gmail.com>
2026-02-06 19:14:05 +0530

    P713

---
+++ src/DataStructureAlgorithm/LEETCODE/P713.java
@@ -0,0 +1,28 @@
+package DataStructureAlgorithm.LEETCODE;
+
+public class P713 {
+        public int numSubarrayProductLessThanK(int[] nums, int k) {
+
+            int n = nums.length;
+            int count=0,j =0,i=0;
+            int product =1;
+
+            while(j<n)
+            {
+
+                product *=nums[j];
+                while (product >= k && i<=j) {
+
+                    product /= nums[i];
+                    i++;
+                }
+
+                count += (j - i + 1);
+                j++;
+
+            }
+            return count;
+
+        }
+    }
+
