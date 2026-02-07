7e1fcc4aebf2547c967dadba48e7de4393e17c1e
Ayush Ojha <ayushojha992005@gmail.com>
2026-02-07 16:14:21 +0530

    p217

---
+++ src/DataStructureAlgorithm/LEETCODE/P217.java
@@ -0,0 +1,28 @@
+package DataStructureAlgorithm.LEETCODE;
+
+public class P217 {
+
+        public boolean containsDuplicate(int[] arr) {
+            HashMap<Integer,Integer> map = new HashMap<>();
+
+            for(int i =0;i<arr.length;i++)
+            {
+                if(map.containsKey(arr[i]))
+                {
+                    int a = map.get(arr[i]);
+                    map.put(arr[i],a+1);
+                }
+                else{
+                    map.put(arr[i],1);
+                }
+
+            }
+            for(int i: map.keySet())
+            {
+                if(map.get(i) > 1) return true;
+            }
+
+            return false;
+        }
+    }
+
