b2148a7779a36708af3e4c1fdefaed31863e99f4
Ayush Ojha <ayushojha992005@gmail.com>
2026-02-09 22:20:32 +0530

    P387

---
+++ src/DataStructureAlgorithm/LEETCODE/P387.java
@@ -0,0 +1,21 @@
+package DataStructureAlgorithm.LEETCODE;
+
+public class P387 {
+
+        public int firstUniqChar(String s) {
+            int[] freq = new int[26];
+
+            // count
+            for(char c : s.toCharArray()) {
+                freq[c - 'a']++;
+            }
+
+            // find first unique
+            for(int i = 0; i < s.length(); i++) {
+                if(freq[s.charAt(i) - 'a'] == 1) return i;
+            }
+
+            return -1;
+        }
+    }
+
