e6a89431851e51e19a2bbb2ab4eb4f040207ce05
Ayush Ojha <ayushojha992005@gmail.com>
2026-02-11 19:56:54 +0530

    P345

---
+++ src/DataStructureAlgorithm/LEETCODE/P345.java
@@ -0,0 +1,41 @@
+package DataStructureAlgorithm.LEETCODE;
+
+public class P345 {
+
+
+        boolean isVowel(char left)
+        {
+            if(left =='a'||left =='e'||left =='i'||left =='o'||left =='u'|| left =='A'||left =='E'||left =='I'||left =='O'||left =='U')
+            { return true;}
+            return false;
+        }
+
+        public String reverseVowels(String s) {
+            int i =0;
+            int j = s.length()-1;
+            char[] s2 =s.toCharArray();
+            while(i<j)
+            {
+                while(i<j && !isVowel(s2[i])){
+                    i++;
+                }
+
+                while(i<j && !isVowel(s2[j])){
+                    j--;
+                }
+
+                char temp = s2[i];
+                s2[i] = s2[j];
+                s2[j] = temp;
+                i++;
+                j--;
+
+
+            }
+
+            return new String(s2);   }
+    }
+
+
+
+
