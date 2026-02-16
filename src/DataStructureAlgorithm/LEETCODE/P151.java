4d43fda5b477846a9aa6d01f5b7b7b024e079ed2
Ayush Ojha <ayushojha992005@gmail.com>
2026-02-16 21:17:40 +0530

    P151

---
+++ src/DataStructureAlgorithm/LEETCODE/P151.java
@@ -0,0 +1,18 @@
+package DataStructureAlgorithm.LEETCODE;
+
+public class P151 {
+
+        public String reverseWords(String s) {
+            String[] words = s.trim().split("\\s+");
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = words.length - 1; i >= 0; i--) {
+                sb.append(words[i]);
+                if (i != 0) sb.append(" ");
+            }
+
+            return sb.toString();
+        }
+    }
+
+
