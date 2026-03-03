925386a16e8834e6aa7495b34c7a4dde1d43a9c1
Ayush Ojha <ayushojha992005@gmail.com>
2026-03-03 11:41:39 +0530

    Largest Substring Between Two Equal Characters

---
+++ src/DataStructureAlgorithm/LEETCODE/P1624.java
@@ -0,0 +1,24 @@
+package DataStructureAlgorithm.LEETCODE;
+
+public class P1624 {
+    class Solution {
+        public int maxLengthBetweenEqualCharacters(String s) {
+            int[] firstIndex = new int[26];
+            Arrays.fill(firstIndex, -1);
+
+            int maxLength = -1;
+
+            for (int i = 0; i < s.length(); i++) {
+                int idx = s.charAt(i) - 'a';
+
+                if (firstIndex[idx] == -1) {
+                    firstIndex[idx] = i;
+                } else {
+                    maxLength = Math.max(maxLength, i - firstIndex[idx] - 1);
+                }
+            }
+
+            return maxLength;
+        }
+    }
+}
