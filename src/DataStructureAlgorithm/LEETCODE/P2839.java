ed6dc64ca2662aed5d0823b6f5c03e9890d05860
Ayush Ojha <ayushojha992005@gmail.com>
2026-03-29 16:47:26 +0530

    P2839

---
+++ src/DataStructureAlgorithm/LEETCODE/P2839.java
@@ -0,0 +1,22 @@
+package DataStructureAlgorithm.LEETCODE;
+
+public class P2839 {
+
+
+    class Solution {
+        public boolean canBeEqual(String s1, String s2) {
+            char[] a1 = new char[]{s1.charAt(0), s1.charAt(2)};
+            char[] a2 = new char[]{s2.charAt(0), s2.charAt(2)};
+
+            char[] b1 = new char[]{s1.charAt(1), s1.charAt(3)};
+            char[] b2 = new char[]{s2.charAt(1), s2.charAt(3)};
+
+            java.util.Arrays.sort(a1);
+            java.util.Arrays.sort(a2);
+            java.util.Arrays.sort(b1);
+            java.util.Arrays.sort(b2);
+
+            return java.util.Arrays.equals(a1, a2) && java.util.Arrays.equals(b1, b2);
+        }
+    }
+}
