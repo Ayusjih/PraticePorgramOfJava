6e8f384efe12d9671d8a848eb7608e745a71c535
Ayush Ojha <ayushojha992005@gmail.com>
2026-05-23 11:21:53 +0530

    110

---
+++ src/DataStructureAlgorithm/Tree/p110.java
@@ -0,0 +1,40 @@
+package DataStructureAlgorithm.Tree;
+
+public class p110 {
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     *     int val;
+     *     TreeNode left;
+     *     TreeNode right;
+     *     TreeNode() {}
+     *     TreeNode(int val) { this.val = val; }
+     *     TreeNode(int val, TreeNode left, TreeNode right) {
+     *         this.val = val;
+     *         this.left = left;
+     *         this.right = right;
+     *     }
+     * }
+     */
+    class Solution {
+
+
+
+        public int solve(TreeNode root)
+        {
+            if(root == null) return 0;
+            int ld = solve(root.left);
+            if(ld==-1) return -1;
+            int rd= solve(root.right);
+            if(rd==-1) return -1;
+            if(Math.abs(ld-rd)>1) return -1;
+            return 1+  Math.max(ld,rd);
+        }
+
+        public boolean isBalanced(TreeNode root) {
+
+            if(solve(root)==-1) return false;
+            return true;
+        }
+    }
+}
